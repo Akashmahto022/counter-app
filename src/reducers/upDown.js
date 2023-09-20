7fa778867b8935429d2808a8b91c8fe9de1cc402
Akash kumar <akashmahto1169@gmail.com>
2023-09-20 09:27:48 +0530

    learn redux with the short project

---
+++ src/reducers/upDown.js
@@ -0,0 +1,23 @@
+const initialState = 0;
+
+const changeTheNumber = (state = initialState, action)=>{
+    switch(action.type){
+        case "Increment": return state + 5;
+        case "Decrement": return state - 1;
+        default : return state
+    }
+
+}
+
+export default changeTheNumber;
+
+
+// if (action.type = "Increment"){
+//     return state + 1
+// }
+// else if(action.type = "Decrement"){
+//     return state - 1
+// }
+// else{
+//     return state
+// }
